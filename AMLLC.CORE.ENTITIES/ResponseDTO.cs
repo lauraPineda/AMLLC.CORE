3ed33c6a4d0b674281bf2a019ac5d442e4c38a15
George <jpach81@gmail.com>
2018-04-13 01:14:02 -0500

    Se agregò dto generico

---
+++ AMLLC.CORE.ENTITIES/ResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace AMLLC.CORE.ENTITIES
+{
+    [DataContract]
+    public class ResponseDTO<T>
+    {
+        [DataMember]
+        public bool Success { get; set; }
+        [DataMember]
+        public string Message { get; set; }
+        [DataMember]
+        public T Result { get; set; }
+    }
+}
