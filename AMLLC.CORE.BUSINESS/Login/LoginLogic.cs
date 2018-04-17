ff4933f610b2bff0c7d681cab1d14929fb34cef2
George <jpach81@gmail.com>
2018-04-17 01:13:03 -0500

    Implementación de WebApi (2.5/3) - Se actualizò controlador y logica

--- AMLLC.CORE.BUSINESS/Login/LoginLogic.cs
+++ AMLLC.CORE.BUSINESS/Login/LoginLogic.cs
@@ -6,7 +6,35 @@ namespace AMLLC.CORE.BUSINESS.Login
 {
     public class LoginLogic
     {
-        public ResponseDTO<LoginDTO> Login(LoginDTO resquest)
+        private static LoginLogic instance;
+
+        /// <summary>
+        /// Constructor privado para ser usado mediante singleton
+        /// </summary>
+        private LoginLogic()
+        {
+            instance = new LoginLogic();
+        }
+
+        /// <summary>
+        /// Obtiene una instancia de la clase LoginLogic mediante Singleton.
+        /// </summary>
+        /// <returns>Retorna una instancia del tipo LoginLogic.</returns>
+        public static LoginLogic GetInstance()
+        {
+            if (Equals(instance, null))
+            {
+                instance = new LoginLogic();
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// Obtiene autorización de usuario.
+        /// </summary>
+        /// <param name="request">Credenciales de login</param>
+        /// <returns>Un objeto respuesto del tipo LoginDTO.</returns>
+        public ResponseDTO<LoginDTO> GetLogin(LoginDTO request)
         {
             
             return new ResponseDTO<LoginDTO>();
--- AMLLC.CORE.WEBAPI/Controllers/LoginController.cs
+++ AMLLC.CORE.WEBAPI/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using AMLLC.CORE.ENTITIES;
+﻿using AMLLC.CORE.BUSINESS.Login;
+using AMLLC.CORE.ENTITIES;
 using AMLLC.CORE.ENTITIES.Login;
 using System.Web.Http;
 
@@ -7,9 +8,25 @@ namespace AMLLC.CORE.WEBAPI.Controllers
     public class LoginController : ApiController
     {
         [HttpPost]
-        public ResponseDTO<LoginDTO> User(LoginDTO request)
+        public ResponseDTO<LoginDTO>GetLogin(LoginDTO request)
         {
             var response = new ResponseDTO<LoginDTO>();
+            try
+            {
+                var loginLogic = LoginLogic.GetInstance();
+                response = loginLogic.GetLogin(request);
+            }
+            catch (System.Data.SqlClient.SqlException exception)
+            {
+                response.Success = false;
+                response.Message = exception.Message;
+            }
+            catch (System.Exception exception)
+            {
+                response.Success = false;
+                response.Message = exception.Message;
+            }
+
             return response;
         }
     }
